840c5126c4836a80d9535fe06457d87112020b75
Durga <128186041+durganju@users.noreply.github.com>
2025-06-07 06:23:55 +0530

    task 1 and task 2 files added

---
+++ Task002.java
@@ -0,0 +1,14 @@
+public class Task002 {
+    public static void add() {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Sum is : " + sum);
+    }
+
+    public static void main(String[] args) {
+        add();
+        add();
+        add();
+    }
+}
