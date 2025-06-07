d2d2d49acb8e83ec456840384b8e05e1b2900242
Durga <128186041+durganju@users.noreply.github.com>
2025-06-07 06:45:22 +0530

    added task3&4

---
+++ Task003.java
@@ -0,0 +1,9 @@
+public class Task003 {
+    public static void main(String[] args){
+        int a= 2;
+        int b= 3;
+        int sum_1 = a + b;
+        System.out.println("Output : " + sum_1);
+    }
+
+}
---
+++ swapnumbers.java
@@ -0,0 +1,12 @@
+public class swapnumbers {
+    public static void main(String[] args){
+    int a= 2;
+    int b= 5;
+
+    System.out.println("Before Swap: a = " + a + ", b =" +b);
+    int temp = a;
+    a = b;
+    b = temp;
+    System.out.println("After Swap: a = " + a + ", b =" +b);
+}
+}
